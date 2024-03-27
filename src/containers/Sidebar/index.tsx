e40264bda95cece19fe3268162c274706e9475ae
João Carlos Borges <joaocarlosifsc@hotmail.com>
2024-03-26 22:25:14 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/joao-carlos-dev.png" />
+    <Titulo fontSize={20}>João Carlos</Titulo>
+  </aside>
+)
+
+export default Sidebar
